dfd8ecdc92a8fe4c56bbb9f061e3509aa6896ce0
skiddmarc <84139731+skiddmarc@users.noreply.github.com>
2025-01-09 18:28:53 -0500

    created new game

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -1,20 +1,50 @@
 #include "raylib.h"
 
-int main()
-{
-	InitWindow(720, 480, "Game");
+int main() {
+	const int screenWidth = 800; 
+	const int screenHeight = 450;
+	
+	InitWindow(screenWidth, screenHeight, "cool game");
 
-	while(!WindowShouldClose())
-	{
-		// UpdateGame();
+	Camera3D camera = { 0 };
+	camera.position = Vector3{ 0.0f, 10.0f, 10.f };
+	camera.target = Vector3{ 0.0f };
+	camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
+	camera.fovy = 45.0f;
+	camera.projection = CAMERA_PERSPECTIVE;
 
+	Vector3 cubePosition = { 0.0f };
+
+	SetTargetFPS(60);
+	// -------------------------------------------------------
+	// main game loop
+	// -------------------------------------------------------
+
+	while (!WindowShouldClose()) {
+		//Update
+
+		//Draw
 		BeginDrawing();
-		ClearBackground(WHITE);
+			
+		ClearBackground(RAYWHITE);
 
-		// DrawGame();
+		BeginMode3D(camera);
 
+		DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+		DrawPlane(Vector3{0,0,0}, Vector2{10, 10}, BLUE);
+		DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+		DrawGrid(10, 1.0f);
+
+		EndMode3D();
+		DrawText("Welcome to the third dimension!", 10, 40, 20, DARKGRAY);
+
+		DrawFPS(10, 10);
+	
 		EndDrawing();
 	}
 
 	CloseWindow();
+
+
 }
